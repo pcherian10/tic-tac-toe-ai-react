bb503e785d1591070db65960c27131a6651bacef
pcherian10 <pcherian10@gmail.com>
2019-02-28 20:58:26 -0500

    added basic profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,12 +1,58 @@
 import React, {Component} from 'react'
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels} from '../styled/Profile'
 
 class Profile extends Component {
 
+    static defaultProps = {
+        user: {
+            email: 'USER_EMAIL',
+            game: [
+                {
+                    winner: true,
+                    createdAt: '02/26/2019',
+                    id: '0001'
+                },
+                {
+                    winner: true,
+                    createdAt: '02/27/2019',
+                    id: '0002'
+                },
+                {
+                    winner: true,
+                    createdAt: '02/28/2019',
+                    id: '0003'
+                },
+            ]
+        }
+    }
     render() {
+        let {email} = this.props.user 
         return (
-        <div>
-           <h2>Profile</h2>
-        </div>
+            <Container>
+                <Name>
+                    {email}
+                </Name>
+                <GameList>
+                    <GameListHeader>
+                        My Games
+                    </GameListHeader>
+                </GameList>
+                <ColumnLabels>
+                    <Column>
+                        Outcome
+                    </Column>
+                    <Column>
+                        Guess
+                    </Column>
+                    <Column>
+                        Guessed Correctly
+                    </Column>
+                    <Column>
+                        Date
+                    </Column>
+                </ColumnLabels>
+                
+            </Container>
         )
     }
 }
--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -52,8 +52,33 @@ class TicTacToe extends Component {
         })
     } 
 
-    move = (marker, index) => {
-        console.log('Move made', marker, index)
+    move = (index, marker) => {
+
+        this.setState( (prevState, props) => {
+            let {gameState, yourTurn, gameOver, winner} = prevState
+            yourTurn = !yourTurn
+            gameState.splice(index, 1, marker)
+            let foundWin = this.winChecker(gameState)
+            if (foundWin) {
+                winner = gameState[foundWin[0]]
+            }
+            if (foundWin || !gameState.includes(false)) {
+                gameOver = true
+            }
+            if (!yourTurn && !gameOver) {
+                this.makeAiMove(gameState)
+            }
+            return {
+                gameState ,
+                yourTurn,
+                gameOver,
+                win: foundWin || false,
+                winner
+            }
+        })
+
+        
+
     }
 
     makeAiMove = (gameState) => {
@@ -64,8 +89,10 @@ class TicTacToe extends Component {
                 openSquares.push(index)
             }
         })
-        let aiMove = openSquares[this.random(0, openSquare.length)]
-        this.move(aiMove, otherMark)
+        let aiMove = openSquares[this.random(0, openSquares.length)]
+        setTimeout(() => {
+            this.move(aiMove, otherMark)
+        }, 1000)
     }
 
     recordGame = () => {
